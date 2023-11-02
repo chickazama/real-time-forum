100a4ea3fecfd44b4ca9b071525805831ea71ae9
Matthew Hope <matt@matthewhope.co.uk>
2023-11-02 18:54:15 +0000

    Validate session when getting user

--- api/GetMyUser.go
+++ api/GetMyUser.go
@@ -6,6 +6,7 @@ import (
 	"matthewhope/real-time-forum/dal"
 	"matthewhope/real-time-forum/transport"
 	"net/http"
+	"strconv"
 )
 
 func GetMyUser(w http.ResponseWriter, r *http.Request) {
@@ -13,8 +14,22 @@ func GetMyUser(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "method not allowed.\n", http.StatusMethodNotAllowed)
 		return
 	}
-	userID, err := auth.GetUserIDFromSessionCookie(r)
+	idCookie, err := r.Cookie("UserID")
 	if err != nil {
+		http.Error(w, "unauthorized.\n", http.StatusUnauthorized)
+		return
+	}
+	userID, err := strconv.Atoi(idCookie.Value)
+	if err != nil {
+		http.Error(w, "bad request.\n", http.StatusBadRequest)
+		return
+	}
+	sessionCookie, err := r.Cookie("Session")
+	if err != nil {
+		http.Error(w, "unauthorized.\n", http.StatusUnauthorized)
+		return
+	}
+	if !auth.ValidateSessionCookie(userID, sessionCookie.Value) {
 		http.Error(w, "unauthorized.\n", http.StatusUnauthorized)
 		return
 	}
