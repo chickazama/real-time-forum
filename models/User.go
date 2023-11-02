e1b7a1e8d75b54c898cc66117e53b2349e73b182
Matthew Hope <matt@matthewhope.co.uk>
2023-11-02 16:25:38 +0000

    Define User model

---
+++ models/User.go
@@ -0,0 +1,12 @@
+package models
+
+type User struct {
+	ID                int
+	Nickname          string
+	Age               string
+	Gender            string
+	FirstName         string
+	LastName          string
+	EmailAddress      string
+	EncryptedPassword string
+}
