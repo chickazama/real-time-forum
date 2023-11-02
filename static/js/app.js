6bfc7c59ff0790b978b10d065482af91eba1f615
Matthew Hope <matt@matthewhope.co.uk>
2023-11-02 22:15:46 +0000

    Add Post component

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,43 @@
 import LoginForm from "./components/LoginForm.js";
+import Post from "./components/Post.js";
 import SignupForm from "./components/SignupForm.js";
 import { renderLoginForm, renderSignupForm } from "./render.js";
 
 customElements.define("signup-form", SignupForm);
 customElements.define("login-form", LoginForm);
+customElements.define("post-element", Post);
 
+let user;
+const posts = new Map();
 
 window.addEventListener("load", async () => {
     let res = await fetch("/api/user");
-    if (res.status == 200) {
-        let json = await res.json();
-        main.innerText = `Welcome ${json.nickname}`;
-        let logout = document.createElement("a");
-        logout.innerText = "Logout";
-        logout.href="/logout";
-        main.appendChild(logout);
-    } else {
+    if (res.status !== 200) {
         renderLoginForm();
+        return;
     }
-})
+    let data = await res.json();
+    main.innerText = `Welcome ${data.nickname}`;
+    let logout = document.createElement("a");
+    logout.innerText = "Logout";
+    logout.href="/logout";
+    main.appendChild(logout);
+    await getPostsAsync();
+
+});
+
+async function getPostsAsync() {
+    const res = await fetch("/api/posts");
+    if (res.status !== 200) {
+        console.log(res.statusText);
+        return;
+    }
+    const data = await res.json();
+    console.log(data);
+    for (const item of data) {
+        console.log(item);
+        const post = new Post(item, null);
+        main.appendChild(post);
+        posts.set(item.id, post);
+    }
+}
---
+++ static/js/components/Post.js
@@ -0,0 +1,145 @@
+export default class Post extends HTMLElement {
+    static observedAttributes = ["active"];
+    shadowRoot;
+    Data;
+    Comments;
+    Socket;
+    constructor(data, socket) {
+        super();
+        this.Data = data;
+        this.Socket = socket;
+        const content =
+        `
+        <link type="text/css" rel="stylesheet" href="./static/css/posts.css" />
+        <div class="post" id="post">
+            <h1>${data.content}</h1>
+            <p>${data.author}</p>
+            <div id="send-comment-container>
+                <input type="text" name="comment-content" placeholder="Type a comment..." />
+                <button id="send-comment" type="button">Send Comment</button>
+            </div>
+            <button id="display-comments-button">Show Comments</button>
+            <div class="comments-container" id="comments-container"></div>
+        </div>
+        `;
+        const template = document.createElement("template");
+        template.innerHTML = content;
+        this.shadowRoot = this.attachShadow({mode: "open"});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    connectedCallback() {
+        console.log("Post component added to DOM");
+        this.shadowRoot.addEventListener("click", showCommentsHandler);
+        this.shadowRoot.addEventListener("click", sendCommentHandler);
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        // console.log(`${name}: ${oldValue} -> ${newValue}`);
+        switch (name) {
+            case "active":
+                switch(newValue) {
+                    case "true":
+                        this.shadowRoot.removeEventListener("click", showCommentsHandler);
+                        this.showCommentsAsync().then( () => {
+                            this.shadowRoot.addEventListener("click", hideCommentsHandler);
+                        });
+                        break;
+                    case "false":
+                        this.hideComments();
+                        this.shadowRoot.removeEventListener("click", hideCommentsHandler);
+                        this.shadowRoot.addEventListener("click", showCommentsHandler);
+                        break;
+                }
+                break;
+        }
+    }
+
+    async showCommentsAsync() {
+        const commentsButton = this.shadowRoot.getElementById("display-comments-button");
+        commentsButton.innerText = "Hide Comments";
+        const commentsContainer = this.shadowRoot.getElementById("comments-container");
+        if (!this.Comments) {
+            this.Comments = [];
+            const data = await this.getCommentsAsync();
+            if (!data) {
+                return;
+            }
+            for (const item of data) {
+                this.Comments.push(item);
+            }
+        } else {
+            console.log("Comments already retrieved");
+        }
+        for (const comment of this.Comments) {
+            const div = document.createElement("div");
+            div.classList.add("comment");
+            div.innerHTML =
+            `
+            <h3>${comment.author}</h3>
+            <p>${comment.content}</h1>
+            <p>${comment.date}</p>
+            `;
+            commentsContainer.appendChild(div);
+        }
+    }
+
+    hideComments() {
+        const commentsButton = this.shadowRoot.getElementById("display-comments-button");
+        commentsButton.innerText = "Show Comments";
+        const commentsContainer = this.shadowRoot.getElementById("comments-container");
+        commentsContainer.innerHTML = "";
+    }
+
+    async getCommentsAsync() {
+        try {
+            const response = await fetch("/api/comments", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Accept": "application/json"
+                },
+                body: new URLSearchParams( 
+                    {
+                        "postID":  this.ID
+                    }
+                )
+            });
+            const result = await response.json();
+            console.log("Success:", result);
+            return result;
+        } catch (error) {
+            console.error("Error:", error);
+            return null;
+        }
+    }
+}
+
+function showCommentsHandler(event) {
+    if (event.target.id != "display-comments-button") {
+        return;
+    }
+    console.log(`'showCommentsHandler' fired: ${event}`);
+    const root = event.target.getRootNode();
+    const host = root.host;
+    host.setAttribute("active", "true");
+}
+
+function hideCommentsHandler(event) {
+    if (event.target.id != "display-comments-button") {
+        return;
+    }
+    console.log(`'showCommentsHandler' fired: ${event}`);
+    const root = event.target.getRootNode();
+    const host = root.host;
+    host.setAttribute("active", "false");
+}
+
+function sendCommentHandler(event) {
+    if (event.target.id != "send-comment-button") {
+        return;
+    }
+    const root = event.target.getRootNode();
+    const host = root.host;
+    host.socket.send()
+}
