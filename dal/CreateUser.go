750cf779770a68765d28f811b07f5934cbd4be5b
Matthew Hope <matt@matthewhope.co.uk>
2023-11-02 16:24:42 +0000

    dal: Add GetUser function

--- dal/CreateUser.go
+++ dal/CreateUser.go
@@ -2,19 +2,25 @@ package dal
 
 import "golang.org/x/crypto/bcrypt"
 
-func CreateUser(nickname, age, gender, firstName, lastName, emailAddress, password string) error {
+func CreateUser(nickname, age, gender, firstName, lastName, emailAddress, password string) (int, error) {
+	var result int
 	queryStr := `INSERT INTO "users" (Nickname, Age, Gender, FirstName, LastName, EmailAddress, EncryptedPassword) VALUES(?, ?, ?, ?, ?, ?, ?);`
 	encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return err
+		return result, err
 	}
 	stmt, err := identityDb.Prepare(queryStr)
 	if err != nil {
-		return err
+		return result, err
 	}
-	_, err = stmt.Exec(nickname, age, gender, firstName, lastName, emailAddress, string(encryptedPassword))
+	res, err := stmt.Exec(nickname, age, gender, firstName, lastName, emailAddress, string(encryptedPassword))
 	if err != nil {
-		return err
+		return result, err
 	}
-	return nil
+	id, err := res.LastInsertId()
+	if err != nil {
+		return result, err
+	}
+	result = int(id)
+	return result, nil
 }
---
+++ dal/GetUserByID.go
@@ -0,0 +1,14 @@
+package dal
+
+import "matthewhope/real-time-forum/models"
+
+func GetUserByID(id int) (models.User, error) {
+	var result models.User
+	queryStr := `SELECT * FROM "users" WHERE ID = (?);`
+	row := identityDb.QueryRow(queryStr, id)
+	err := row.Scan(&result.ID, &result.Nickname, &result.Age, &result.Gender, &result.FirstName, &result.LastName, &result.EmailAddress, &result.EncryptedPassword)
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
