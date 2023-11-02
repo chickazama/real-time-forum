a2c0ed77af4367319deda5d746d9fdf343920512
Matthew Hope <matt@matthewhope.co.uk>
2023-11-02 15:14:15 +0000

    Define input transport errors

---
+++ transport/errors.go
@@ -0,0 +1,12 @@
+package transport
+
+const (
+	INVALID_INPUT_NICK_NAME      = "invalid input nick name"
+	INVALID_INPUT_FIRST_NAME     = "invalid input first name"
+	INVALID_INPUT_LAST_NAME      = "invalid input last name"
+	INVALID_INPUT_PASSWORD       = "invalid input password"
+	INVALID_INPUT_AGE            = "invalid input age"
+	INVALID_INPUT_GENDER         = "invalid input gender"
+	INVALID_INPUT_EMAIL          = "invalid input email address"
+	INVALID_INPUT_DURING_SIGN_IN = "invalid input during sign in"
+)
